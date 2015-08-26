c227806bdd851b8c2d78afe0aedab43395705795
Ini Oguntola <ini@mit.edu>
2015-08-26 19:15:30 -0400

    Updated github url

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='AMP',
-    version='1.1.3',
+    version='1.1.4',
     author='Ini Oguntola',
     author_email='ioguntol@gmail.com',
     packages=['AMP', 'AMP.test'],
     scripts=[],
-    url='http://github.com/ioguntol/Automatic-Mathematical-Parser/',
+    url='http://github.com/ioguntol/amp/',
     license='LICENSE.txt',
-    description='Automatic Mathematical Parser.',
+    description='A Python 2.7 mathematical parsing library',
     long_description=open('README.txt').read(),
     install_requires=[
         "Django >= 1.1.1",
